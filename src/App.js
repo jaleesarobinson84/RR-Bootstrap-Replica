f1cb19956c2e11c3606b275f210bc36cd864446d
Jaleesa Robinson <robinson.jaleesa42@gmail.com>
2024-02-29 11:25:21 -0500

    insert image, and finished up assignment

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import './App.css';
-import Navbar from 'react-bootstrap/Navbar'
-import Nav from 'react-bootstrap/Nav'
-import Button from 'react-bootstrap/Button'
-import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Button from 'react-bootstrap/Button';
+import {useState, useRef} from 'react';
+import Overlay from 'react-bootstrap/Overlay';
 import StoreList from './components/StoreListing';
+import Image from 'react-bootstrap/Image'
 
 
 
 function App() {
+  const [show, setShow] = useState(false);
+  const target = useRef(null);
+
   return (
     <div className="App">
+      <Image src="https://www.istockphoto.com/photo/multi-colored-vegetables-and-spices-frame-on-black-slate-background-overhead-view-gm1272587212-374810493?utm_campaign=srp_photos_top&utm_content=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Ffree-food-backgrounds&utm_medium=affiliate&utm_source=unsplash&utm_term=free+food+backgrounds%3A%3A%3A" fluid />;
       <Navbar bg="light" expand="lg">
         <Navbar.Brand style={{ margin: "5px 0 0 20px" }} href="#home">
           <img src="https://www.instacart.com/image-server/x24/www.instacart.com/assets/beetstrap/brand/2022/instacart-logo-color@3x-6b71df83cfba8c6827f59bff009df3be6e96d484ebdc5da7e6122e9555eae9b6.png" alt="Instacart Logo" />
@@ -23,6 +29,36 @@ function App() {
           </Nav>
         </Navbar.Collapse>
       </Navbar>
+
+      <>
+      <Button variant="danger" ref={target} onClick={() => setShow(!show)}>
+        Welcome to Virtual Shopping!
+      </Button>
+      <Overlay target={target.current} show={show} placement="right">
+        {({
+          placement: _placement,
+          arrowProps: _arrowProps,
+          show: _show,
+          popper: _popper,
+          hasDoneInitialMeasure: _hasDoneInitialMeasure,
+          ...props
+        }) => (
+          <div
+            {...props}
+            style={{
+              position: 'absolute',
+              backgroundColor: 'rgba(255, 100, 100, 0.85)',
+              padding: '2px 10px',
+              color: 'white',
+              borderRadius: 3,
+              ...props.style,
+            }}
+          >
+            Simple tooltip
+          </div>
+        )}
+      </Overlay>
+    </>
 
       <StoreList /> {/* Render the StoreList component here */}
 
